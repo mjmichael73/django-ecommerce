e4c7c80fe041adf097247d782d2d1944a2822871
Mojtaba Michael <mojimich2015@gmail.com>
2024-03-21 15:01:19 +0300

    Added get absolute url for shop models

--- be/ecommerce/shop/models/product_model.py
+++ be/ecommerce/shop/models/product_model.py
@@ -1,5 +1,6 @@
 from django.db import models
 from shop.models import Category
+from django.urls import reverse
 
 
 class Product(models.Model):
@@ -30,3 +31,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
