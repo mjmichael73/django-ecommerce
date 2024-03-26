b6923a9dcc5f60aa288ed59f75a03ad2fdacc2c0
Mojtaba Michael <mojimich2015@gmail.com>
2024-03-26 12:38:30 +0300

    Completed cart.py in cart app

--- be/ecommerce/cart/cart.py
+++ be/ecommerce/cart/cart.py
@@ -14,6 +14,31 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
+    def __iter__(self):
+        """
+        Iterate over the items in the cart and get the products from the database.
+        :return:
+        """
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        """
+        Count all items in the cart
+        :return:
+        """
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def save(self):
+        self.session.modified = True
+
     def add(self, product, quantity=1, override_quantity=False):
         """
         Add a product to the cart or update its quantity.
@@ -22,4 +47,27 @@ class Cart:
         :param override_quantity:
         :return:
         """
-        pass
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def remove(self, product):
+        """
+        Remove a product from the cart.
+        :param product:
+        :return:
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
