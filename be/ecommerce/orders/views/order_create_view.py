b766f1877d2afd0a7cb78d630c869c51fb419bc2
Mojtaba Michael <mojimich2015@gmail.com>
2024-04-02 19:02:50 +0300

    Changed order_create_view to redirect to payment process page

--- be/ecommerce/orders/views/order_create_view.py
+++ be/ecommerce/orders/views/order_create_view.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from orders.models import OrderItem
 from orders.forms import OrderCreateForm
 from cart.utils import Cart
@@ -20,13 +21,8 @@ def order_create(request):
                 )
             cart.clear()
             order_created.delay(order.id)
-        return render(
-            request,
-            "orders/order/created.html",
-            {
-                "order": order
-            }
-        )
+            request.session['order_id'] = order.id
+            return redirect(reverse('payment:process'))
     else:
         form = OrderCreateForm()
     return render(
